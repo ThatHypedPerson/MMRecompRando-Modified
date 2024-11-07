20562cc8bf5684527a681bb5adba7ea9979e684f
LittleCube <littlecubehax@gmail.com>
2024-11-07 11:50:58 -0500

    add code to get both shooting checks to swamp

--- src/shooting_gallery_hooks.c
+++ src/shooting_gallery_hooks.c
@@ -191,39 +191,42 @@ static Vec3f sTownPlayerPos = { -20.0f, 40.0f, 175.0f };
 
 void EnSyatekiMan_Town_RunGame(EnSyatekiMan* this, PlayState* play);
 
-//~ RECOMP_PATCH void EnSyatekiMan_Town_StartGame(EnSyatekiMan* this, PlayState* play) {
-    //~ static s16 sGameStartTimer = 30;
-    //~ Player* player = GET_PLAYER(play);
+RECOMP_PATCH void EnSyatekiMan_Town_StartGame(EnSyatekiMan* this, PlayState* play) {
+    static s16 sGameStartTimer = 30;
+    Player* player = GET_PLAYER(play);
 
-    //~ if (sGameStartTimer == 30) {
-        //~ if (player->transformation == PLAYER_FORM_FIERCE_DEITY) {
-            //~ player->actor.world.pos = sTownFierceDeityPlayerPos;
-        //~ } else {
-            //~ player->actor.world.pos = sTownPlayerPos;
-        //~ }
+    extraItem = 0;
+    offeringItem = 0;
 
-        //~ player->actor.prevPos = player->actor.world.pos;
-        //~ player->actor.shape.rot.y = -0x8000;
-        //~ player->actor.world.rot.y = player->actor.shape.rot.y;
-        //~ play->unk_18790(play, -0x8000);
-        //~ player->stateFlags1 |= PLAYER_STATE1_20;
-        //~ sGameStartTimer--;
-    //~ } else if (sGameStartTimer > 0) {
-        //~ player->actor.shape.rot.y = -0x8000;
-        //~ player->actor.world.rot.y = player->actor.shape.rot.y;
-        //~ sGameStartTimer--;
-    //~ } else if (sGameStartTimer == 0) {
-        //~ player->stateFlags1 &= ~PLAYER_STATE1_20;
-        //~ this->score = 50;
-        //~ this->flagsIndex = 0;
-        //~ this->octorokState = SG_OCTO_STATE_INITIAL;
-        //~ this->lastHitOctorokType = SG_OCTO_TYPE_NONE;
-        //~ sGameStartTimer = 30;
-        //~ Interface_StartTimer(TIMER_ID_MINIGAME_1, 75);
-        //~ this->actor.draw = NULL;
-        //~ this->actionFunc = EnSyatekiMan_Town_RunGame;
-    //~ }
-//~ }
+    if (sGameStartTimer == 30) {
+        if (player->transformation == PLAYER_FORM_FIERCE_DEITY) {
+            player->actor.world.pos = sTownFierceDeityPlayerPos;
+        } else {
+            player->actor.world.pos = sTownPlayerPos;
+        }
+
+        player->actor.prevPos = player->actor.world.pos;
+        player->actor.shape.rot.y = -0x8000;
+        player->actor.world.rot.y = player->actor.shape.rot.y;
+        play->unk_18790(play, -0x8000);
+        player->stateFlags1 |= PLAYER_STATE1_20;
+        sGameStartTimer--;
+    } else if (sGameStartTimer > 0) {
+        player->actor.shape.rot.y = -0x8000;
+        player->actor.world.rot.y = player->actor.shape.rot.y;
+        sGameStartTimer--;
+    } else if (sGameStartTimer == 0) {
+        player->stateFlags1 &= ~PLAYER_STATE1_20;
+        this->score = 50;
+        this->flagsIndex = 0;
+        this->octorokState = SG_OCTO_STATE_INITIAL;
+        this->lastHitOctorokType = SG_OCTO_TYPE_NONE;
+        sGameStartTimer = 30;
+        Interface_StartTimer(TIMER_ID_MINIGAME_1, 75);
+        this->actor.draw = NULL;
+        this->actionFunc = EnSyatekiMan_Town_RunGame;
+    }
+}
 
 void EnSyatekiMan_Town_Talk(EnSyatekiMan* this, PlayState* play);
 
@@ -279,6 +282,8 @@ RECOMP_PATCH void EnSyatekiMan_Town_EndGame(EnSyatekiMan* this, PlayState* play)
     }
 }
 
+// Below code is for Swamp Shooting:
+
 #define SG_SWAMP_PERFECT_SCORE_WITHOUT_BONUS                                                     \
     (SG_POINTS_DEKU_NORMAL * (SG_SWAMP_DEKUS_PER_WAVE * SG_SWAMP_WAVE_COUNT) +                   \
      SG_POINTS_GUAY * (SG_SWAMP_GUAYS_PER_WAVE * SG_SWAMP_WAVE_COUNT) +                          \
@@ -287,12 +292,54 @@ RECOMP_PATCH void EnSyatekiMan_Town_EndGame(EnSyatekiMan* this, PlayState* play)
 
 #define SG_SWAMP_HEART_PIECE_SCORE (SG_SWAMP_PERFECT_SCORE_WITHOUT_BONUS + (6 * SG_BONUS_POINTS_PER_SECOND))
 
+static Vec3f sSwampPlayerPos = { 0.0f, 10.0f, 140.0f };
+
+void EnSyatekiMan_Swamp_RunGame(EnSyatekiMan* this, PlayState* play);
+
+RECOMP_PATCH void EnSyatekiMan_Swamp_StartGame(EnSyatekiMan* this, PlayState* play) {
+    static s16 sGameStartTimer = 30;
+    Player* player = GET_PLAYER(play);
+
+    extraItem = 0;
+    offeringItem = 0;
+
+    if (sGameStartTimer > 0) {
+        player->actor.world.pos = sSwampPlayerPos;
+        player->actor.shape.rot.y = -0x8000;
+        player->actor.world.rot.y = player->actor.shape.rot.y;
+        play->unk_18790(play, -0x8000);
+        sGameStartTimer--;
+    } else {
+        sGameStartTimer = 30;
+        this->flagsIndex = 0;
+        this->score = 0;
+        player->stateFlags1 &= ~PLAYER_STATE1_20;
+        Actor_PlaySfx(&this->actor, NA_SE_SY_FOUND);
+        this->dekuScrubFlags = (1 << 4) | (1 << 3) | (1 << 2) | (1 << 1) | (1 << 0);
+        this->guayFlags = 0;
+        this->wolfosFlags = 0;
+        this->guayAppearTimer = 0;
+        this->dekuScrubHitCounter = 0;
+        this->guayHitCounter = 0;
+        this->currentWave = 0;
+        this->bonusDekuScrubHitCounter = 0;
+        Interface_StartTimer(TIMER_ID_MINIGAME_1, 100);
+        this->actor.draw = NULL;
+        this->actionFunc = EnSyatekiMan_Swamp_RunGame;
+    }
+}
+
 void EnSyatekiMan_Swamp_GiveReward(EnSyatekiMan* this, PlayState* play);
 
 RECOMP_PATCH void EnSyatekiMan_Swamp_SetupGiveReward(EnSyatekiMan* this, PlayState* play) {
     Player* player = GET_PLAYER(play);
 
+    if (offeringItem != 0) {
+        Actor_OfferGetItem(&this->actor, play, offeringItem, 500.0f, 100.0f);
+    }
+
     if (Actor_HasParent(&this->actor, play)) {
+        offeringItem = 0;
         if (!CHECK_WEEKEVENTREG(WEEKEVENTREG_RECEIVED_SWAMP_SHOOTING_GALLERY_QUIVER_UPGRADE)) {
             SET_WEEKEVENTREG(WEEKEVENTREG_RECEIVED_SWAMP_SHOOTING_GALLERY_QUIVER_UPGRADE);
         } else if (!CHECK_WEEKEVENTREG(WEEKEVENTREG_RECEIVED_SWAMP_SHOOTING_GALLERY_HEART_PIECE) &&
@@ -301,22 +348,28 @@ RECOMP_PATCH void EnSyatekiMan_Swamp_SetupGiveReward(EnSyatekiMan* this, PlaySta
         }
 
         this->actor.parent = NULL;
-        this->actionFunc = EnSyatekiMan_Swamp_GiveReward;
-    } else {
-        if ((CUR_UPG_VALUE(UPG_QUIVER) < 3) &&
-            !CHECK_WEEKEVENTREG(WEEKEVENTREG_RECEIVED_SWAMP_SHOOTING_GALLERY_QUIVER_UPGRADE)) {
-            Actor_OfferGetItem(&this->actor, play, GI_QUIVER_50, 500.0f, 100.0f);
-        } else if (this->score < SG_SWAMP_HEART_PIECE_SCORE) {
-            Actor_OfferGetItem(&this->actor, play, GI_RUPEE_RED, 500.0f, 100.0f);
-        } else if (!CHECK_WEEKEVENTREG(WEEKEVENTREG_RECEIVED_SWAMP_SHOOTING_GALLERY_HEART_PIECE)) {
-            Actor_OfferGetItemHook(&this->actor, play, rando_get_item_id(LOCATION_SWAMP_GALLERY_PERFECT), LOCATION_SWAMP_GALLERY_PERFECT, 500.0f, 100.0f, true, true);
+        if (extraItem == 0) {
+            this->actionFunc = EnSyatekiMan_Swamp_GiveReward;
         } else {
-            Actor_OfferGetItem(&this->actor, play, GI_RUPEE_PURPLE, 500.0f, 100.0f);
+            offeringItem = extraItem;
+            extraItem = 0;
         }
-
-        player->actor.shape.rot.y = -0x8000;
-        player->actor.velocity.z = 0.0f;
-        player->actor.velocity.x = 0.0f;
-        player->actor.world.rot.y = player->actor.shape.rot.y;
+    } else if (offeringItem == 0) {
+        //~ if ((CUR_UPG_VALUE(UPG_QUIVER) < 3) &&
+            //~ !CHECK_WEEKEVENTREG(WEEKEVENTREG_RECEIVED_SWAMP_SHOOTING_GALLERY_QUIVER_UPGRADE)) {
+        if (!rando_location_is_checked(LOCATION_SWAMP_GALLERY_PERFECT)) {
+            Actor_OfferGetItemHook(&this->actor, play, rando_get_item_id(LOCATION_SWAMP_GALLERY_PERFECT), LOCATION_SWAMP_GALLERY_PERFECT, 500.0f, 100.0f, true, true);
+            if (!rando_location_is_checked(GI_QUIVER_50)) {
+                extraItem = GI_QUIVER_50;
+            }
+        //~ } else if (this->score < SG_SWAMP_HEART_PIECE_SCORE) {
+        } else if (this->score >= SG_SWAMP_HEART_PIECE_SCORE) {
+            Actor_OfferGetItem(&this->actor, play, GI_RUPEE_HUGE, 500.0f, 100.0f);
+        //~ } else if (!CHECK_WEEKEVENTREG(WEEKEVENTREG_RECEIVED_SWAMP_SHOOTING_GALLERY_HEART_PIECE)) {
+        } else if (!rando_location_is_checked(GI_QUIVER_50)) {
+            Actor_OfferGetItem(&this->actor, play, GI_QUIVER_50, 500.0f, 100.0f);
+        } else {
+            Actor_OfferGetItem(&this->actor, play, GI_RUPEE_RED, 500.0f, 100.0f);
+        }
     }
 }
