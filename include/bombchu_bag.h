9a01b06f8ca497a2edb82c25db677c99ef784d2d
LittleCube <littlecubehax@gmail.com>
2024-12-19 04:03:05 -0500

    attempt to import bombchu bag

---
+++ include/bombchu_bag.h
@@ -0,0 +1,13 @@
+#ifndef BOMBCHU_BAG_H
+#define BOMBCHU_BAG_H 1
+
+extern u64 bombchu_bag_zobjTex_000000[];
+extern u64 bombchu_bag_zobjTLUT_000200[];
+extern u64 bombchu_bag_zobjTLUT_000238[];
+extern Vtx bombchu_bag_zobjVtx_000478[];
+extern Gfx gGiBombchu0DL[];
+extern Vtx bombchu_bag_zobjVtx_000818[];
+extern Gfx gGiBombchu1DL[];
+extern Vtx bombchu_bag_zobjVtx_0009C0[];
+extern Gfx gGiBombchu2DL[];
+#endif
